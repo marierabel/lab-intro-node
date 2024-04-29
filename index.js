12cd9a421df03644b61f17f5bd11f2561216d28d
Marie Rabel <marie.rabel0697@gmail.com>
2024-04-29 15:18:25 +0200

    lab fini

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort((a, b) => a - b);
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items.splice(pos, 1);
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let max = 0;
+      for (const it of this.items) {
+        if (it > max) {
+          max = it;
+        }
+      }
+      return max;
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      let max = this.items[0];
+      for (const it of this.items) {
+        if (it < max) {
+          max = it;
+        }
+      }
+      return max;
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    for (const it of this.items) {
+      sum += it;
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    let sum = 0;
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      for (const it of this.items) {
+        sum += it;
+      }
+      let avg = sum / this.length;
+      return avg;
+    }
+  }
 }
 
 module.exports = SortedList;
